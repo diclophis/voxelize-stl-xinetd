848ed3a7be4185b2c67bce8623f6ac53126d6369
Jon Bardin <diclophis@gmail.com>
2016-01-04 22:01:18 -0800

    smaller square

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-X=$(seq -1 1 -1)
-Y=$(seq -1 1 -1)
+X=$(seq -2 1 2)
+Y=$(seq -2 1 2)
 TMP=/tmp
 
 while true
@@ -11,10 +11,10 @@ for i in $X
 do
   for j in $Y
   do
-    r=0 #$(echo $RANDOM % 4 | bc)
-    s=0 #$(echo $RANDOM % 4 | bc)
-    k=$(echo $i '*' 128 + $r | bc)
-    j=$(echo $j '*' 128 + $s | bc)
+    r=0
+    s=0
+    k=$(echo $i '*' 32 + $r | bc)
+    j=$(echo $j '*' 32 + $s | bc)
     read stl
     echo "/home/mavencraft/voxelizer/build/bin/voxelizer 254 4 $TMP/$stl $TMP/$stl.vox && rm $TMP/$stl && (/home/mavencraft/voxelizer/build/test/testVox $TMP/$stl.vox | ruby /home/mavencraft/voxelize-stl-xinetd/main.rb $k $j) && rm $TMP/$stl.vox"
   done
